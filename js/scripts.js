1d7c086c9d546898f88df2a4fcd706405e375d55
Liz <jesusridesthebus@gmail.com>
2019-04-05 12:50:40 -0700

    add more js logic, still in the process of getting it to push results to array

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,28 @@ var boop = 'Boop!';
 var dave = 'I\'m sorry Dave. I\'m afraid I can\'t do that.'; 
 
 function returnPhrase(number) {
-	var input = $('input#beep-boop-input').val(number);
-	var outputResult = '';
 	var outputArray = [];
 	
+	
+	for (var i = 0; i <= number; i++) {
+		var outputResult = '';
+		if (number.includes(1)) {
+			outputArray(outputResult += 'Beep!').push;
+		}
+	}
+	console.log(outputArray);
 }
 
 $(function() {
-	$('form#beep-boop-input').submit(function(event) {
+	$('form#beep-boop').submit(function(event) {
 		event.preventDefault();
-		$('.beep-boop-result').hide();
-		var numberInput = parseInt($('#beep-boop-input').val());
+		$('#result').hide();
+		var numberInput = parseInt($('input#beep-boop-input').val());
 		var result = returnPhrase(numberInput);
 		$('.beep-boop-result').text(result);
+		$('#result').show(result);
 	});
 });
+
+
+//outputArray.push('' + number) --changes to string ?
