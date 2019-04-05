83b92438b4152d60482fe2af4f691a81c18f6cae
Liz <jesusridesthebus@gmail.com>
2019-04-05 11:33:19 -0700

    add beginning of back end logic in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
+var beep = 'Beep!';
+var boop = 'Boop!';
+var dave = 'I\'m sorry Dave. I\'m afraid I can\'t do that.'; 
 
+function returnPhrase(number) {
+	var input = $('input#beep-boop-input').val(number);
+	var outputResult = '';
+	var outputArray = [];
+	
+}
 
 $(function() {
-  $('form#beep-boop-input').submit(function(event) {
-    event.preventDefault();
-    $('.beep-boop-result').hide();
-    var numberInput = parseInt($("#beep-boop-input").val());
-    var result = returnPhrase(numberInput);
-    $(".beep-boop-result").text(result);
-  });
+	$('form#beep-boop-input').submit(function(event) {
+		event.preventDefault();
+		$('.beep-boop-result').hide();
+		var numberInput = parseInt($('#beep-boop-input').val());
+		var result = returnPhrase(numberInput);
+		$('.beep-boop-result').text(result);
+	});
 });
