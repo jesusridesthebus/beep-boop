a53bf5de4e886f2a481e6d5513d6918428a9d5ea
Liz <jesusridesthebus@gmail.com>
2019-04-06 12:37:41 -0700

    remove second for loop in js and additionally refactor. also add a slice to remove very last comma in displayed array

--- js/scripts.js
+++ js/scripts.js
@@ -4,27 +4,24 @@ function returnPhrase(number) {
 	var i = 0;
 	for (i = 0; i <= number; i++) {
 		if(i.toString().includes('3')) {
-			resultArray.push('I\'m sorry Dave. I\'m afraid I can\'t do that.');
+			resultArray.push('I\'m sorry Dave. I\'m afraid I can\'t do that. ');
 		} else if(i.toString().includes('2')) {
-			resultArray.push('Boop!');
+			resultArray.push('Boop! ');
 		} else if(i.toString().includes('1')) {
-			resultArray.push('Beep!');
+			resultArray.push('Beep! ');
 		} else {
-			resultArray.push(i);
+			resultArray.push(i + ', ');
 		}
-	}
-
-	for (i = 0; i < resultArray.length; i++) {
-		$('#result').append('' + resultArray[i] + ', ');
-	};
+	} return resultArray.join('');
 }
 
 $(function() {
 	$('form#beep-boop').submit(function(event) {
 		event.preventDefault();
-		var numberInput = parseInt($('input#beep-boop-input').val());
+		var numberInput = $('input#beep-boop-input').val();
+		var result = returnPhrase(numberInput);
 		$('#result').empty();
-		returnPhrase(numberInput);
+		$('#result').text(result.slice(0, -2));
 		$('#result').show();
 	});
 });
