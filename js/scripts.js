c4bfcaf5a83eed765442b1adb13d30bdef02a4d8
Liz <jesusridesthebus@gmail.com>
2019-04-05 19:16:41 -0700

    remove unnecessary span from html, refact js by adjusting main for loop and removing another one, removing hide result, removing unneeded .text(result), remove extraneous spans

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,8 @@
 function returnPhrase(number) {
-	var outputArray = [];
 	var resultArray = [];
+		
 	var i = 0;
-
 	for (i = 0; i <= number; i++) {
-		outputArray.push(i);
-	}
-
-	for (i = 0; i < outputArray.length; i++) {
 		if(i.toString().includes('3')) {
 			resultArray.push('I\'m sorry Dave. I\'m afraid I can\'t do that.');
 		} else if(i.toString().includes('2')) {
@@ -19,19 +14,17 @@ function returnPhrase(number) {
 		}
 	}
 
-	for (i = 0; i < outputArray.length; i++) {
-		$('#result').append('<span>' + resultArray[i] + ', </span>');
+	for (i = 0; i < resultArray.length; i++) {
+		$('#result').append('' + resultArray[i] + ', ');
 	};
 }
 
 $(function() {
 	$('form#beep-boop').submit(function(event) {
 		event.preventDefault();
-		$('#result').hide();
 		var numberInput = parseInt($('input#beep-boop-input').val());
 		$('#result').empty();
-		var result = returnPhrase(numberInput);
-		$('.beep-boop-result').text(result);
+		returnPhrase(numberInput);
 		$('#result').show();
 	});
 });
